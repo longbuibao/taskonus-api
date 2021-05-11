26d4e6c9a7bb46c9d0ffba50abdd52c0c873f1c2
long bui <blong1102@gmail.com>
2021-05-11 20:02:23 +0700

    chap 78 challenge

--- mongodb.js
+++ mongodb.js
@@ -3,16 +3,16 @@ const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
-
-MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, result) => {
-    if (err) return console.log('There is an error');
-    //database to connect
-    const db = result.db(databaseName);
-    //collection to use
-    db.collection('users').insertMany([{
-        task: 'clean your house',
-        isDone: true
-    }], (err, res) => {
-        console.log(res.ops)
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, result) => {
+        if (err) return console.log('There is an error');
+        const database = result.db(databaseName);
+        database.collection('users').find({ age: { $gt: 2 } }).toArray((_, res) => {
+            console.log(res)
+        })
+        database.collection('users').findOne(new mongodb.ObjectID('609a4689ada6e4d00a280aa6'),
+            (_, res) => {
+                console.log(res)
+            }
+        )
     })
-})
