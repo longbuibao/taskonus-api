c36083ffad444fc16b67529fd158cefbb696b214
long bui <blong1102@gmail.com>
2021-05-16 15:06:25 +0700

    chap 93

---
+++ src/playground.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Task = require('../src/models/task')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(res => console.log('connected to the database'))
+    .catch(e => console.log(e))
+
+async function removeTask(id) {
+    const result = await Task.findByIdAndRemove(id)
+    if (result) {
+        console.log("removed " + id + " task")
+    } else {
+        console.log("cannot find " + id + " or it was removed")
+    }
+}
+async function findAllIncompleteTasks() {
+    const numberOfIncompleteTasks = await Task.countDocuments({ completed: false })
+    console.log(`number Of Incomplete Tasks: ${numberOfIncompleteTasks}`)
+}
+
+removeTask('60a0cac8d178b33f8ccdd0f9')
+findAllIncompleteTasks()
