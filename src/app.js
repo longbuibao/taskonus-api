11c5c767bdb9bb0335d9f26bf37a63ea1cda87b6
long bui <62802719+longbuibao@users.noreply.github.com>
2021-06-04 16:30:45 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/'. (_, res)=>{
+app.get('/', (_, res)=>{
   res.send('<p>Please go to this <a href="https://github.com/longbuibao/taskApp">link </a></p> to have more information about this API')
 })
 
